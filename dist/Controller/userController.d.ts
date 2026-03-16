70fd7f26df2a26c022b2e88d268132b6cdd8ca8d
unknown <adenugaa942@gmail.com>
2026-03-16 20:55:26 +0100

    Remove .env and node modules

--- dist/Controller/userController.d.ts
+++
@@ -1,4 +0,0 @@
-import { NextFunction, Request, Response } from "express";
-export declare const createUser: (req: Request, res: Response, next: NextFunction) => Promise<Response<any, Record<string, any>> | undefined>;
-export declare const loginUser: (req: Request, res: Response) => Promise<Response<any, Record<string, any>> | undefined>;
-//# sourceMappingURL=userController.d.ts.map
